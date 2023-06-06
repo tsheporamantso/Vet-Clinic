51d7c1d6834c5c21a66e8cb318512c28387682dc
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-06 13:16:28 +0200

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-/* Database schema to keep the structure of entire database. */
+/* Day 1 used queries. */
+CREATE TABLE animals ( id BIGSERIAL NOT NULL PRIMARY KEY,
+                                                     name VARCHAR(255) NOT NULL,
+                                                                       date_of_birth DATE NOT NULL,
+                                                                                          escape_attempt INTEGER DEFAULT 0,
+                                                                                                                         neutered BOOLEAN DEFAULT FALSE,
+                                                                                                                                                  weight_kg DECIMAL(5,2) NOT NULL);
 
-CREATE TABLE animals (
-    name varchar(100)
-);
