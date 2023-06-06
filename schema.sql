8ea4eef9291226bdab0cc0d612466e5c67314b79
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-06 17:16:37 +0200

    Rectify typo on escape attempts

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 CREATE TABLE animals (id BIGSERIAL NOT NULL PRIMARY KEY,
                                                     name VARCHAR(255) NOT NULL,
                                                                       date_of_birth DATE NOT NULL,
-                                                                                         escape_attempt INTEGER DEFAULT 0,
-                                                                                                                        neutered BOOLEAN DEFAULT FALSE,
-                                                                                                                                                 weight_kg DECIMAL(5,2) NOT NULL);
+                                                                                         escape_attempts INTEGER DEFAULT 0,
+                                                                                                                         neutered BOOLEAN DEFAULT FALSE,
+                                                                                                                                                  weight_kg DECIMAL(5,2) NOT NULL);
 
