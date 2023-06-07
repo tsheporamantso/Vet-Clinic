98ea60a52dfd5d5c68310a9a810a8ce699ffcb24
tsheporamantso <tshepo.ramantso@outlook.com>
2023-06-07 22:55:32 +0200

    Add owners, species table and alter animals table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,24 @@ CREATE TABLE animals (
 /* Day 2 queries and update animals table */
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+/* Day 2 query multiple tables */
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INTEGER REFERENCES species(id),
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+ALTER TABLE animals
+ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
